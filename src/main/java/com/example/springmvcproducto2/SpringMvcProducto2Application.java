3254cb71b431b95333f04fcf8bb739f96fcf2e10
alu10711862 <alurod1862@ieselcaminas.org>
2023-12-18 19:55:48 +0100

    V1

---
+++ src/main/java/com/example/springmvcproducto2/SpringMvcProducto2Application.java
@@ -0,0 +1,13 @@
+package com.example.springmvcproducto2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcProducto2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcProducto2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springmvcproducto2/SpringMvcProducto2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvcproducto2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcProducto2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
